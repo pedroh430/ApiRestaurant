7e359e00f631b50f8391ea703122ce27b41d5680
pedro <pedrobldap41@gmail.com>
2025-11-04 23:45:30 -0300

    service

--- src/main/java/com/example/ApiTest/Entity/Dishes.java
+++ src/main/java/com/example/ApiTest/Entity/Dishes.java
@@ -2,18 +2,23 @@ package com.example.ApiTest.Entity;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Dishes {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
 
     private String Name;
     private Double Price;
     private String Description;
 
+    public Dishes() {
+    }
 
     public Dishes(Integer Id, String Name, Double Price, String Description) {
 
---
+++ src/main/java/com/example/ApiTest/Service/DishesService.java
@@ -0,0 +1,26 @@
+package com.example.ApiTest.Service;
+
+import com.example.ApiTest.Entity.Dishes;
+import com.example.ApiTest.Repository.DishesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DishesService {
+
+    private final DishesRepository dishesRepository;
+
+
+    public DishesService(DishesRepository dishesRepository) {
+        this.dishesRepository = dishesRepository;
+    }
+
+    public List<Dishes> getALLDishes(){
+        return dishesRepository.findAll();
+    }
+
+    public void insertDishes(Dishes dishes){
+        dishesRepository.save(dishes);
+    }
+}
